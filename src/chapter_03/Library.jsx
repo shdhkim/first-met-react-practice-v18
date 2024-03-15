d626eec96b22e1060d324139b345a3dacdf0a8c4
eiose <49316394+shdhkim@users.noreply.github.com>
2024-03-15 18:46:35 +0900

    <Book name="처음 만난 파이썬" numOfPage={200} />             <Book name="처음 만난 AWS" numOfPage={430} />           <Book name="처음 만난 리액트" numOfPage={210} />

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -5,8 +5,8 @@ function Library(props) {
     return (
         <div>
             <Book name="처음 만난 파이썬" numOfPage={200} />
-            <Book name="처음 만난 AWS" numOfPage={400} />
-          <Book name="처음 만난 리액트" numOfPage={200} />
+            <Book name="처음 만난 AWS" numOfPage={430} />
+          <Book name="처음 만난 리액트" numOfPage={210} />
 
            
 
