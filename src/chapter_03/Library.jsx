0e73989c0cc52470c10c3496581f31f5e8abd00b
eiose <49316394+shdhkim@users.noreply.github.com>
2024-03-15 13:07:29 +0900

    600->700

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -6,7 +6,7 @@ function Library(props) {
         <div>
             <Book name="처음 만난 파이썬" numOfPage={300} />
             <Book name="처음 만난 AWS" numOfPage={400} />
-            <Book name="처음 만난 리액트" numOfPage={600} />
+            <Book name="처음 만난 리액트" numOfPage={700} />
         </div>
     );
 }
