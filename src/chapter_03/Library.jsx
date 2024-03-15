876c9dd78535925aa610ecf94948f559462a8eea
eiose <49316394+shdhkim@users.noreply.github.com>
2024-03-15 18:49:29 +0900

    no message

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -4,15 +4,12 @@ import Book from "./Book";
 function Library(props) {
     return (
         <div>
-<<<<<<< HEAD
+
             <Book name="처음 만난 파이썬" numOfPage={200} />
             <Book name="처음 만난 AWS" numOfPage={430} />
           <Book name="처음 만난 리액트" numOfPage={210} />
-=======
-            <Book name="처음 만난 파이썬" numOfPage={100} />
-            <Book name="처음 만난 AWS" numOfPage={200} />
-          <Book name="처음 만난 리액트" numOfPage={300} />
->>>>>>> version1
+
+        
 
            
 
