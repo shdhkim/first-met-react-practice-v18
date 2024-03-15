9128c706d018e2d79bfa48384e67c3ff8c40d13d
eiose <49316394+shdhkim@users.noreply.github.com>
2024-03-15 16:18:42 +0900

    300->200

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -4,7 +4,7 @@ import Book from "./Book";
 function Library(props) {
     return (
         <div>
-            <Book name="처음 만난 파이썬" numOfPage={300} />
+            <Book name="처음 만난 파이썬" numOfPage={200} />
             <Book name="처음 만난 AWS" numOfPage={400} />
             <Book name="처음 만난 리액트" numOfPage={600} />
         </div>
