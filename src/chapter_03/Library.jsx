ee708701271c83cbdae43c935ded90f18b7e1224
eiose <49316394+shdhkim@users.noreply.github.com>
2024-03-15 16:37:16 +0900

    no message

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -6,11 +6,11 @@ function Library(props) {
         <div>
             <Book name="처음 만난 파이썬" numOfPage={200} />
             <Book name="처음 만난 AWS" numOfPage={400} />
-<<<<<<< HEAD
+
             <Book name="처음 만난 리액트" numOfPage={100} />
-=======
-            <Book name="처음 만난 리액트" numOfPage={900} />
->>>>>>> master
+
+           
+
         </div>
     );
 }
